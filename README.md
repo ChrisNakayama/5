a76adceefad4988ad4640f844391d5e51f24f0f1
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-11-15 20:30:17 -0800

    Second commit added JS Logic and functionality
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>
    Co-Authored-by: Michael Munyaneza <kumachiz@gmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Co-Authored-by: Michael Munyaneza <kumachiz@gmail.com>
 Co-authored-by: Ebru Rice <ebruerenb@gmail.com>
 CODE PROJECT
 
+Spec Sheet 
+
+It should take in an input from a user
+
+It should take out all punctuation and make the input lower case
+
+It should be able to find the end of the string
+
+It should start at index 0 
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,56 @@
+//Business Logic
+
 function cryptosquare(text) {
-  
+  const cryptotext = '';
+  const letterArray = text.toLowerCase.split('');
+  const cryptoArray = letterArray.remove('.',',',"'",'!','?',';',':','-',);
+  for (let i = 0; i <= cryptoArray.length; i+=8);
+    if (i === 0) {
+    cryptotext.push(letterArray[i]);
+    } else if (i%8===1) {
+      cryptotext.push(letterArray[i]);
+    } else if (i%8===2) {
+      cryptotext.push(letterArray[i]);
+    } else if (i%8===3) {
+      cryptotext.push(letterArray[i]);
+    } else if (i%8===4) {
+      cryptotext.push(letterArray[i]);
+    } else if (i%8===5) {
+      cryptotext.push(letterArray[i]);
+    } else if (i%8===6) {
+      cryptotext.push(letterArray[i]);
+    } else if (i%8===7) {
+      cryptotext.push(letterArray[i]);
+    } else {
+      cryptotext.push(letterArray[i]);
+    }
+    console.log(cryptotext);
+    console.log(letterArray);
+    console.log(cryptoArray);
+    return cryptotext
 }
 
+//UI Logic
+
+$(document).ready(function(){
+  $('form#crypto').submit(function(event){
+    event.preventDefault();
+    const passage = $('#text').val();
+    const cryptoPassage = cryptosquare(passage);
+    $('#result').html(cryptoPassage);
+
+
+    
+
+
+//code for the alphabet
+
+// function isLetter(str) {
+//   return str.length === 1 && str.match(/[a-z]/i);
+// }
 
 
 //code sample
-
 // function splitArrayIntoChunksOfLen(arr, len) {
 //   var chunks = [], i = 0, n = arr.length;
 //   while (i < n) {
