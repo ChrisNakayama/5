46572563f3061586e7cd3fb9c9597f4ccd989ffd
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-11-15 18:53:25 -0800

    First Commit Group Co-Author
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>
    Co-Authored-by: Michael Munyaneza <kumachiz@gmail.com>
    Co-authored-by: Ebru Rice <ebruerenb@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+Group PROJECT WITH TEAM
+Co-Authored-by: Chris Nakayama <cnakayam@gmail.com>
+Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>
+Co-Authored-by: Michael Munyaneza <kumachiz@gmail.com>
+Co-authored-by: Ebru Rice <ebruerenb@gmail.com>
+CODE PROJECT
+
---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+function cryptosquare(text) {
+  
+}
+
+
+
+//code sample
+
+// function splitArrayIntoChunksOfLen(arr, len) {
+//   var chunks = [], i = 0, n = arr.length;
+//   while (i < n) {
+//     chunks.push(arr.slice(i, i += len));
+//   }
+//   return chunks;
+// }
+// var alphabet=['a','b','c','d','e','f'];
+// var alphabetPairs=splitArrayIntoChunksOfLen(alphabet,2); //split into chunks of two
+
+//code sample
